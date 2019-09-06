22f5ea2d8133bf1d98051a5b0672cd2775bae0a1
ateyodin <52425179+ateyodin@users.noreply.github.com>
2019-09-06 19:56:45 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# Name what im about to do, count Chikens
+print("I will now count my chickens:")
+# Counting hens  30 divided 6 + 25
+print("Hens", 25.0 + 30.0 / 6.0)
+# Counting roosters first 25 times 3 then modular of 75 % 4 them 100 - 3
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# Count the egss
+print("Now i will count the eggs:")
+# first 1 divided by 4 that 0.25 then 4 % 2 thats 0 then do regular operators
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+# We do a question
+print("Is it true that 3.0 + 2/0 < 5.0 - 7.0?")
+#this is false cause 5 is not < that - 2
+print(3.0 + 2.0 < 5.0 - 7.0)
+# 3 + 2 equals to 5
+print("What is 3 + 2?", 3.0 + 2.0)
+# 5 - 7 equals - 2
+print("What is 5 - 7?", 5.0 - 7.0)
+#thats the reason
+print("Oh, that's why it's False.")
+# more comments
+print("How about some more.")
+# 5 is greater than -2
+print("Is it greater?", 5.0 > -2.0)
+# 5 still greater that 2
+print("Is it greater or equal?", 5.0 >= -2.0)
+# 5 is not smaller than -2
+print("Is it less or equal?", 5.0 <= -2.0)
